fed2056f7daa003092bb57e3de2b53b800b40e7c
Paul Zaich <pzaich@gmail.com>
2012-06-16 16:48:05 -0700

    Initial commit to repository.

---
+++ zaichfamily/_/js/functions.js
@@ -0,0 +1,23 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+	alert("I am an alert box!");
+	
+	
+
+});
+
+
+
+
+$(window).load(function() {
+});
+
+$(window).resize(function() {
+	
+});
+
+*/
