a2b944a2de772f27ce3c0deaed06bbbc974581cf
Paul Zaich <pzaich@gmail.com>
2012-08-23 07:13:42 +0900

    category page formatting

--- zaichfamily/archive.php
+++ zaichfamily/archive.php
@@ -5,25 +5,25 @@
  			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
 			<?php /* If this is a category archive */ if (is_category()) { ?>
-				<h2>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
+				<h1><?php single_cat_title(); ?></h1>
 
 			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-				<h2>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
+				<h1>Posts Tagged <?php single_tag_title(); ?></h1>
 
 			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h2>Archive for <?php the_time('F jS, Y'); ?></h2>
+				<h1>Archive for <?php the_time('F jS, Y'); ?></h1>
 
 			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h2>Archive for <?php the_time('F, Y'); ?></h2>
+				<h1>Archive for <?php the_time('F, Y'); ?></h1>
 
 			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h2 class="pagetitle">Archive for <?php the_time('Y'); ?></h2>
+				<h1 class="pagetitle">Archive for <?php the_time('Y'); ?></h1>
 
 			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h2 class="pagetitle">Author Archive</h2>
+				<h1 class="pagetitle">Author Archive</h1>
 
 			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h2 class="pagetitle">Blog Archives</h2>
+				<h1 class="pagetitle">Blog Archives</h1>
 			
 			<?php } ?>
 
@@ -31,17 +31,34 @@
 
 			<?php while (have_posts()) : the_post(); ?>
 			
-				<article <?php post_class() ?>>
+				<div class="article-wrapper"?>
+						<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+							
+							<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+							
+							<div class="floatright thumbnail">
+								<?php if (has_post_thumbnail()){ ?>
+									<?php the_post_thumbnail(); } ?>
+							</div>
+								
+							<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
 				
-						<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-					
-						<?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?>
-
-						<div class="entry">
-							<?php the_content(); ?>
-						</div>
-
-				</article>
+							<div class="entry">
+								
+								<?php the_excerpt(); ?>
+								
+							</div>
+							
+								
+							<footer class="postmetadata">
+								<!--<?php the_tags('Tags: ', ', ', '<br />'); ?>-->
+								<!--Posted in <?php the_category(', ') ?> |--> 
+								<!--<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>-->
+							</footer>
+				
+						</article>
+					</div>
+				
 
 			<?php endwhile; ?>
 
